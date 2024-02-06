ac17106446482dec128bc72ee45c874bba84f929
c00ki <55160446+ejcook23@users.noreply.github.com>
2024-02-06 10:28:38 -0700

    get and set board

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -15,7 +15,7 @@ public class ChessGame {
 
     }
 
-    private ChessBoard board;
+    private ChessBoard chessBoard;
     private TeamColor teamColor;
     private TeamColor teamTurn;
     /**
@@ -72,8 +72,17 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        boolean inCheck = false;
+
+
+
+        return inCheck;
     }
+
+
+
+
+
 
     /**
      * Determines if the given team is in checkmate
@@ -102,7 +111,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        chessBoard = board;
     }
 
     /**
@@ -111,6 +120,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return chessBoard;
     }
 }
