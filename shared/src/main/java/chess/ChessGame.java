abf301cdc5b2eae53889dc1936142c9b4d04d039
c00ki <55160446+ejcook23@users.noreply.github.com>
2024-02-07 20:40:01 -0700

    implemented validMoves, and I think it works!

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -51,8 +51,40 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        throw new RuntimeException("Not implemented");
         Collection<ChessMove> possibleMoves = new ArrayList<>();
+        ChessPiece currPiece = chessBoard.getPiece(startPosition);
+
+        // check if there is a piece there
+        if(currPiece == null) {
+            return null;
+        }
+
+        // iterate through all possible moves of that piece
+        for(ChessMove move : currPiece.pieceMoves(chessBoard,startPosition)) {
+
+            //gets my teams color
+            TeamColor color = currPiece.getTeamColor();
+            //gets END POSITION of the move and saves it
+            ChessPosition endPos = move.getEndPosition();
+            // gets the PIECE at the END position and saves it
+            ChessPiece eatenPiece = chessBoard.getPiece(endPos);
+            // moves piece to the new position
+            chessBoard.addPiece(endPos,currPiece);
+            // set old position to null
+            chessBoard.addPiece(startPosition, null);
+
+            // checks to see if that puts us in check, and if not, adds it to the list of possible moves
+            if(!isInCheck(color)) {
+                possibleMoves.add(move);
+            }
+
+            //moves piece back and places back old piece (if there was one)
+            chessBoard.addPiece(startPosition, currPiece);
+            chessBoard.addPiece(endPos, eatenPiece);
+
+        }
+
+        return possibleMoves;
     }
 
     /**
@@ -73,9 +105,9 @@ public class ChessGame {
      */
     public boolean isInCheck(TeamColor teamColor) {
         boolean inCheck = false;
-        ChessPiece findPiece = new ChessPiece(teamColor, ChessPiece.PieceType.KING);
+        ChessPiece piece = new ChessPiece(teamColor, ChessPiece.PieceType.KING);
         ChessPosition kingPosition;
-        kingPosition = findPiece(findPiece);
+        kingPosition = findPiece(piece);
 
         // goes through all the board spots
         aBreak:
@@ -83,10 +115,10 @@ public class ChessGame {
             for(int col = 1; col > 0 && col < 9;) {
                 // if it is an enemy color and not null
                 ChessPosition testPos = new ChessPosition(row,col) ;
-                if((chessBoard.getPiece(testPos).getTeamColor() != teamColor) && (chessBoard.getPiece(testPos) != null)) {
+                if((chessBoard.getPiece(testPos) != null) && (chessBoard.getPiece(testPos).getTeamColor() != teamColor)) {
                     // see if any of its end positions are on the king spot
                     for(ChessMove move : chessBoard.getPiece(testPos).pieceMoves(chessBoard,testPos)) {
-                        inCheck = (move.getEndPosition() == testPos);
+                        inCheck = (move.getEndPosition().equals(kingPosition));
                         if(inCheck) {
                             break aBreak;
                         }
@@ -99,12 +131,12 @@ public class ChessGame {
         return inCheck;
     }
 
-    private ChessPosition findPiece(ChessPiece findPiece) {
+    private ChessPosition findPiece(ChessPiece piece) {
         ChessPosition piecePos = null;
         bigBreak:
         for(int row = 1; row > 0 && row < 9;) {
             for(int col = 1; col > 0 && col < 9;) {
-                if(chessBoard.getPiece(new ChessPosition(row,col)) == findPiece) {
+                if(chessBoard.getPiece(new ChessPosition(row,col)) != null && chessBoard.getPiece(new ChessPosition(row,col)).equals(piece)) {
                     piecePos = new ChessPosition(row,col);
                     break bigBreak;
                 }
