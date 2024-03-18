b7683dfb92646bc38d748626a9866473e1eafc7a
c00ki <55160446+ejcook23@users.noreply.github.com>
2024-03-18 15:32:08 -0600

    working on Server Facade

---
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -0,0 +1,39 @@
+package clientTests;
+
+import org.junit.jupiter.api.*;
+import server.Server;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ServerFacadeTests {
+
+    private static Server server;
+    static ServerFacade facade;
+
+    @BeforeAll
+    public static void init() {
+        server = new Server();
+        var port = server.run(0);
+        System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
+    }
+
+    @AfterAll
+    static void stopServer() {
+        server.stop();
+    }
+
+
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void register() throws Exception {
+        var authData = facade.register("player1", "password", "p1@email.com");
+        assertTrue(authData.authToken().length() > 10);
+    }
+
+}
