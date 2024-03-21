a610088a02b2112328d63131a6a20c21d97c5212
c00ki <55160446+ejcook23@users.noreply.github.com>
2024-03-20 19:46:19 -0600

    gave the chess board letters and numbers on the sides lets gooooo

--- client/src/main/java/ui/ChessBoard.java
+++ client/src/main/java/ui/ChessBoard.java
@@ -3,6 +3,8 @@ package ui;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import ui.*;
 import chess.*;
@@ -21,8 +23,8 @@ public class ChessBoard {
     static ArrayList<String> boardArray = new ArrayList<>();
     static ArrayList<String> colorArray = new ArrayList<>();
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        run();
     }
 
     public static void run() throws Exception {
@@ -136,12 +138,29 @@ public class ChessBoard {
 
     public static void printBoard(PrintStream out, Boolean whiteOnBottom, ArrayList<String> boardArray, ArrayList<String> colorArray) {
         int rowCounter = 0;
-
-
+        int rowCounterRev = 8;
+        List<String> messages = Arrays.asList("a", "b", "c", "d", "e","f","g","h");
 
         // DRAW BOARD
         if (whiteOnBottom) {
+
+            // LETTERS TOP
+            printSquare(out,"grey","uhh", EMPTY);
+            for(String m : messages) {
+                printSquare(out,"grey","uhh", " " + m + " ");
+            }
+            printSquare(out,"grey","uhh", EMPTY);
+
+            out.print(SET_BG_COLOR_DARK_GREY + "\n");
+
+
             for (int counter = boardArray.size() - 1; counter >= 0; counter--) {
+                // LEFT DESCENDING NUM
+                if(counter%8 == 7) {
+                    printSquare(out,"grey","uhh", " " + (rowCounterRev) + " ");
+                }
+
+                // CENTER BOARD
                 if(counter%2 == 0) {
                     if(rowCounter%2 == 0) {
                         printSquare(out,"BLACK",colorArray.get(counter),boardArray.get(counter));
@@ -157,15 +176,38 @@ public class ChessBoard {
                     }
 
                 }
+
+                // RIGHT DESCENDING NUM
                 if(counter%8 == 0) {
+                    printSquare(out,"grey","uhh", " " + (rowCounterRev) + " ");
                     out.print(SET_BG_COLOR_DARK_GREY + "\n");
                     rowCounter++;
+                    rowCounterRev--;
                 }
             }
 
+            // LETTERS BOTTOM
+            printSquare(out,"grey","uhh", EMPTY);
+            for(String m : messages) {
+                printSquare(out,"grey","uhh", " " + m + " ");
+            }
+            printSquare(out,"grey","uhh", EMPTY);
+            out.print(SET_BG_COLOR_DARK_GREY + "\n");
+
         } else {
 
+            printSquare(out,"grey","uhh", EMPTY);
+            for(int i = messages.size()-1; i >= 0; i--) {
+                printSquare(out,"grey","uhh", " " + messages.get(i) + " ");
+            }
+            printSquare(out,"grey","uhh", EMPTY);
+            out.print(SET_BG_COLOR_DARK_GREY + "\n");
+
+
             for (int counter = 0; counter <= boardArray.size() - 1; counter++) {
+                if(counter%8 == 0) {
+                    printSquare(out,"grey","uhh", " " + (rowCounter+1) + " ");
+                }
                 if(counter%2 == 0) {
                     if(rowCounter%2 == 0) {
                         printSquare(out,"WHITE",colorArray.get(counter),boardArray.get(counter));
@@ -182,14 +224,25 @@ public class ChessBoard {
 
                 }
                 if(counter%8 == 7) {
+                    printSquare(out,"grey","uhh", " " + (rowCounter+1) + " ");
                     out.print(SET_BG_COLOR_DARK_GREY + "\n");
                     rowCounter++;
+                    rowCounterRev--;
                 }
             }
+
+            printSquare(out,"grey","uhh", EMPTY);
+            for(int i = messages.size()-1; i >= 0; i--) {
+                printSquare(out,"grey","uhh", " " + messages.get(i) + " ");
+            }
+            printSquare(out,"grey","uhh", EMPTY);
+            out.print(SET_BG_COLOR_DARK_GREY + "\n");
 
         }
 
         out.print(SET_TEXT_COLOR_WHITE);
+
+
     }
 
 
@@ -201,14 +254,17 @@ public class ChessBoard {
         } else if (Objects.equals(squareColor.toUpperCase(), "WHITE")){
             out.print(SET_BG_COLOR_WHITE);
         } else {
-            out.print(SET_BG_COLOR_DARK_GREY);
+            out.print(SET_BG_COLOR_LIGHT_GREY);
         }
 
         // SET TEAM COLOR
         if(Objects.equals(teamColor.toUpperCase(), "BLACK")) {
             out.print(SET_TEXT_COLOR_RED);
-        } else {
+
+        } else if (Objects.equals(teamColor.toUpperCase(), "WHITE")){
             out.print(SET_TEXT_COLOR_BLUE);
+        } else {
+            out.print(SET_TEXT_COLOR_WHITE);
         }
 
         // PRINT CONTENT
